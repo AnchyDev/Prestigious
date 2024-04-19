b4826f87525ca49b1d93eed9f61f07f14bd41e30
AnchyDev <lewienato@gmail.com>
2024-04-19 12:38:13 +1000

    Fix codestyle check.

--- src/scripts/PrestigiousWorld.cpp
+++ src/scripts/PrestigiousWorld.cpp
@@ -9,7 +9,7 @@ void PrestigiousWorldScript::OnUpdate(uint32 diff)
 void PrestigiousWorldScript::OnAfterConfigLoad(bool /*reload*/)
 {
     sPrestigeHandler->LoadRewards();
-	sPrestigeHandler->LoadPrestigeLevels();
+    sPrestigeHandler->LoadPrestigeLevels();
 }
 
 void PrestigiousWorldScript::OnShutdownInitiate(ShutdownExitCode /*code*/, ShutdownMask /*mask*/)
