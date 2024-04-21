b0d7a0751d9e260efc8ac6fcff4b2da91206d4b7
AnchyDev <lewienato@gmail.com>
2024-04-21 12:25:52 +1000

    Make prestige text red for elvui.

--- src/scripts/PrestigiousNPC.cpp
+++ src/scripts/PrestigiousNPC.cpp
@@ -53,8 +53,8 @@ bool PrestigiousNPCScript::OnGossipSelect(Player* player, Creature* creature, ui
         auto itemLevel = player->GetAverageItemLevel();
         bool isDeathKnight = player->getClass() == CLASS_DEATH_KNIGHT;
 
-        AddGossipItemFor(player, GOSSIP_ICON_CHAT, Acore::StringFormatFmt("I would like to keep my equipment|n|cff170066({}x Multiplier)|r", sPrestigeHandler->GetBaseMultiplier(isDeathKnight)), GOSSIP_SENDER_MAIN, PRESTIGE_DO_PRESTIGE, "Are you sure you would like to prestige?|n|nThis will reset your level and quest progress.", 0, false);
-        AddGossipItemFor(player, GOSSIP_ICON_CHAT, Acore::StringFormatFmt("I would like to sacrifice my equipment|n|cff170066({} Item Level, {}x Multiplier)|r", itemLevel, sPrestigeHandler->GetMultiplierForItemLevel(itemLevel, isDeathKnight)), GOSSIP_SENDER_MAIN, PRESTIGE_DO_PRESTIGE_SACRIFICE, "Are you sure you would like to prestige?|n|nThis will reset your level and progress.|n|n|cffFF0000THIS OPTION WILL DELETE YOUR EQUIPMENT, YOU CANNOT UNDO THIS", 0, false);
+        AddGossipItemFor(player, GOSSIP_ICON_CHAT, Acore::StringFormatFmt("I would like to keep my equipment|n|cffFF0000({}x Multiplier)|r", sPrestigeHandler->GetBaseMultiplier(isDeathKnight)), GOSSIP_SENDER_MAIN, PRESTIGE_DO_PRESTIGE, "Are you sure you would like to prestige?|n|nThis will reset your level and quest progress.", 0, false);
+        AddGossipItemFor(player, GOSSIP_ICON_CHAT, Acore::StringFormatFmt("I would like to sacrifice my equipment|n|cffFF0000({} Item Level, {}x Multiplier)|r", itemLevel, sPrestigeHandler->GetMultiplierForItemLevel(itemLevel, isDeathKnight)), GOSSIP_SENDER_MAIN, PRESTIGE_DO_PRESTIGE_SACRIFICE, "Are you sure you would like to prestige?|n|nThis will reset your level and progress.|n|n|cffFF0000THIS OPTION WILL DELETE YOUR EQUIPMENT, YOU CANNOT UNDO THIS", 0, false);
 
         SendGossipMenuFor(player, PRESTIGE_TEXT_OPTIONS, creature);
     }
